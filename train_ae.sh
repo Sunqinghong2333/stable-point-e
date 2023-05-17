24a1664470277be3f74d3dea6d4c5d3f86ddb72e
Sunqinghong2333 <sunqinghong@bupt.edu.cn>
2023-05-17 15:29:28 +0800

    UploadText

--- train_ae.sh
+++ train_ae.sh
@@ -1,4 +1,4 @@
-
+yyy
 
 srun -p DBX16 --quotatype=spot --gres=gpu:1 --ntasks=1 --ntasks-per-node=1 \
 -x SH-IDC1-10-198-8-[107,238,255,244,246,247,248,249,250,252,153,240],SH-IDC1-10-198-9-[1,12,10] \
